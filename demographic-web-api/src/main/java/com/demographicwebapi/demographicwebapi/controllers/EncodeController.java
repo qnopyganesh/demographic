b99d0929e4cf4322d211f5c6f50192a79b53ae7a
manish81690 <manishgupta81690@gmail.com>
2022-08-24 15:42:04 +0530

    Removed Unused Libraries

--- demographic-web-api/src/main/java/com/demographicwebapi/demographicwebapi/controllers/EncodeController.java
+++ demographic-web-api/src/main/java/com/demographicwebapi/demographicwebapi/controllers/EncodeController.java
@@ -1,16 +1,12 @@
 package com.demographicwebapi.demographicwebapi.controllers;
-
-// import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-// import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
+
 
 import com.demographicwebapi.demographicwebapi.services.AlgoService;
 
