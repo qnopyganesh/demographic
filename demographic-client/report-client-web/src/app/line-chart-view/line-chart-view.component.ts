07afbf888072cb1a0ea1551b00e6bcba29878731
Jainish Sakhidas <jainishsakhidas@gmail.com>
2022-11-18 18:00:44 +0530

    Fixed charts

--- demographic-client/report-client-web/src/app/line-chart-view/line-chart-view.component.ts
+++ demographic-client/report-client-web/src/app/line-chart-view/line-chart-view.component.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
-import { FileReadingService } from 'app/service/filereading.service';
+import { HttpClient } from "@angular/common/http";
+import { Component, Input, OnInit, ViewChild } from "@angular/core";
+import { FileReadingService } from "app/service/filereading.service";
 import {
   ChartComponent,
   ApexAxisChartSeries,
@@ -9,7 +9,7 @@ import {
   ApexDataLabels,
   ApexTitleSubtitle,
   ApexStroke,
-  ApexGrid
+  ApexGrid,
 } from "ng-apexcharts";
 
 export type ChartOptions = {
@@ -22,21 +22,22 @@ export type ChartOptions = {
   title: ApexTitleSubtitle;
 };
 @Component({
-  selector: 'app-line-chart-view',
-  templateUrl: './line-chart-view.component.html',
-  styleUrls: ['./line-chart-view.component.scss']
+  selector: "app-line-chart-view",
+  templateUrl: "./line-chart-view.component.html",
+  styleUrls: ["./line-chart-view.component.scss"],
 })
-export class LineChartViewComponent implements OnInit{
-  @ViewChild("baraccuracychart", { static: true }) baraccuracychart: ChartComponent;
-  public type:ApexChart;
+export class LineChartViewComponent implements OnInit {
+  @ViewChild("baraccuracychart", { static: true })
+  baraccuracychart: ChartComponent;
+  public type: ApexChart;
   public chartOptions: Partial<ChartOptions>;
-  public namefile:Map<string,any>;
-  public key:string = "Caverphone";
-  public ready:boolean = false;
-  public selectedAlgorithm:string;
-  @Input() source:string;
-  @Input() title:string;
-  @Input() public radioName:string;
+  public namefile: Map<string, any>;
+  public key: string = "Caverphone";
+  public ready: boolean = false;
+  public selectedAlgorithm: string;
+  @Input() source: string;
+  @Input() title: string;
+  @Input() public radioName: string;
   public algorithms = [
     { value: "Caverphone1" },
     { value: "Caverphone2" },
@@ -44,7 +45,7 @@ export class LineChartViewComponent implements OnInit{
     { value: "DaitchMokotoffSoundex" },
     { value: "ColognePhonetic" },
     { value: "DoubleMetaphone" },
-    {value : "DaitchMokotoffSoundex"},
+    { value: "DaitchMokotoffSoundex" },
     { value: "MatchRatingApproachEncoder" },
     { value: "Metaphone" },
     { value: "Metaphone3" },
@@ -53,123 +54,137 @@ export class LineChartViewComponent implements OnInit{
     { value: "Soundex" },
   ];
 
-
   public x = [];
   public y = [];
-  series:ApexAxisChartSeries;
-  constructor(private fileservice:FileReadingService,private httpclient:HttpClient) {
+  series: ApexAxisChartSeries;
+  constructor(
+    private fileservice: FileReadingService,
+    private httpclient: HttpClient
+  ) {
     this.type = {
       height: 350,
       type: "bar",
       zoom: {
-        enabled: true
-      }
+        enabled: true,
+      },
     };
-    this.series = [
+    (this.series = [
       {
         name: this.key,
-        data: [1,2,3,40]
-      }
-    ],
-    this.initializechart();
+        data: [1, 2, 3, 40],
+      },
+    ]),
+      this.initializechart();
   }
-  initializechart(){
+  initializechart() {
     this.baraccuracychart = new ChartComponent();
 
     this.chartOptions = {
       series: this.series,
       chart: this.type,
       dataLabels: {
-        enabled: false
+        enabled: false,
       },
       stroke: {
-        curve: "straight"
+        curve: "straight",
       },
       title: {
         text: this.title,
-        align: "left"
+        align: "left",
       },
       grid: {
         row: {
           colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
-          opacity: 0.5
-        }
+          opacity: 0.5,
+        },
       },
       xaxis: {
-        categories: [1,2,4,5]
-      }
+        categories: [1, 2, 4, 5],
+      },
     };
-    if(this.ready){
-      this.reset1();
+    if (this.ready) {
+      // this.reset1();
     }
-    
   }
 
-  reset1(){
+  reset1() {
     this.chartOptions.series[0].data = this.namefile.get(this.key)[1];
     this.chartOptions.series[0].name = this.key;
     this.chartOptions.xaxis.categories = this.namefile.get(this.key)[0];
   }
   ngOnInit(): void {
     let namefile = new Map();
-    this.httpclient.get(this.source,{responseType:'text'})
-    .subscribe(
-      data => {
+    this.httpclient.get(this.source, { responseType: "text" }).subscribe(
+      (data) => {
         let csvToRowArray = data.split("\n");
         for (let index = 1; index < csvToRowArray.length - 1; index++) {
           let row = csvToRowArray[index].split(",");
-          if(!namefile.has(row[0])){
-            namefile.set(row[0],[[Number(row[1]).toFixed(3)],[Number(row[2].replace('\r','')).toFixed(3)]]);
-          }
-          else{
+          if (!namefile.has(row[0])) {
+            namefile.set(row[0], [
+              [Number(row[1]).toFixed(3)],
+              [Number(row[2].replace("\r", "")).toFixed(3)],
+            ]);
+          } else {
             namefile.get(row[0])[0].push(Number(row[1]).toFixed(3));
-            namefile.get(row[0])[1].push(Number(row[2].replace('\r','')).toFixed(3));
+            namefile
+              .get(row[0])[1]
+              .push(Number(row[2].replace("\r", "")).toFixed(3));
           }
         }
         this.namefile = namefile;
         this.ready = true;
         this.series = [];
-        namefile.forEach((value,key)=>{
-          if(key != 'Caverphone')
-          {this.series.push({
-            name: key,
-            data: value[1]
-          });}
-        })
+        namefile.forEach((value, key) => {
+          if (key != "Caverphone") {
+            this.series.push({
+              name: key,
+              data: value[1],
+            });
+          }
+        });
         this.initializechart();
       },
-    error => {
-    }
+      (error) => {}
     );
-    
   }
-  reset(event){
+  reset(event) {
     this.initializechart();
   }
-  changeCharttoBar(){
-    // ((document.getElementsByName(this.radioName)[0]) as HTMLInputElement).checked = true;
+  changeCharttoBar() {
+    var btn = document.getElementsByClassName(this.radioName);
+
+    if (!btn[0].classList.contains("btn-primary")) {
+      btn[0].classList.add("btn-primary");
+    }
+
+    if (btn[1].classList.contains("btn-primary")) {
+      btn[1].classList.remove("btn-primary");
+    }
 
     this.type = {
       height: 350,
       type: "bar",
       zoom: {
-        enabled: true
-      }
-    }
+        enabled: true,
+      },
+    };
     this.initializechart();
-
   }
-  changeCharttoLine(){
-    // ((document.getElementsByName(this.radioName)[1]) as HTMLInputElement).checked = true;
+  changeCharttoLine() {
+    var btn = document.getElementsByClassName(this.radioName);
+
+    btn[1].classList.add("btn-primary");
+    if (btn[0].classList.contains("btn-primary")) {
+      btn[0].classList.remove("btn-primary");
+    }
+
     this.type = {
       height: 350,
       type: "line",
       zoom: {
-        enabled: true
-      }
-    }
+        enabled: true,
+      },
+    };
     this.initializechart();
-
-
   }
 }
